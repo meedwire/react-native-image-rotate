1f016f10fb51b51b3eb460724c694851caa50d85
leonardo.jeronimo <leonardo.jeronimo@pebmed.com.br>
2023-02-07 18:17:10 -0300

    chore: add example in readme

--- README.md
+++ README.md
@@ -10,12 +10,43 @@ npm install react-native-image-rotate
 
 ## Usage
 
-```js
-import { multiply } from 'react-native-image-rotate';
+```tsx
+import React from 'react';
 
-// ...
+import { StyleSheet, View, Image, Button } from 'react-native';
+import { rotate } from 'react-native-image-rotate';
+import image from './image';
 
-const result = await multiply(3, 7);
+export default function App() {
+  const [result, setResult] = React.useState<string>();
+
+  const rotateImage = async () => {
+    try {
+      const path = await rotate({ type: 'base64', content: image, angle: 90 });
+
+      setResult(path);
+    } catch (error) {}
+  };
+
+  return (
+    <View style={styles.container}>
+      {result && <Image source={{ uri: result }} style={styles.image} />}
+      <Button title="Rotate" onPress={rotateImage} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  image: {
+    alignSelf: 'center',
+    width: 200,
+    height: 200,
+  },
+});
 ```
 
 ## Contributing
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,34 +1,24 @@
-import React, { useCa } from 'react';
+import React from 'react';
 
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Image, Button } from 'react-native';
 import { rotate } from 'react-native-image-rotate';
 import image from './image';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [result, setResult] = React.useState<string>();
 
   const rotateImage = async () => {
     try {
       const path = await rotate({ type: 'base64', content: image, angle: 90 });
 
-      console.log('Path', path);
-
       setResult(path);
-    } catch (error) {
-      console.log(error);
-    }
+    } catch (error) {}
   };
-
-  React.useEffect(() => {
-    rotateImage();
-  }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
-      {result && (
-        <Image source={{ uri: result }} style={{ width: 200, height: 250 }} />
-      )}
+      {result && <Image source={{ uri: result }} style={styles.image} />}
+      <Button title="Rotate" onPress={rotateImage} />
     </View>
   );
 }
@@ -38,9 +28,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  image: {
+    alignSelf: 'center',
+    width: 200,
+    height: 200,
   },
 });
